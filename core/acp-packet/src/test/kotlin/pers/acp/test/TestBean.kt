4d5df33e92fe2c96ca127f8cd7a7387a2793d5a9
zhangbin1010 <391289241@qq.com>
2020-08-26 22:06:53 +0800

    [Upgrade] 增加单元测试代码

---
+++ core/acp-packet/src/test/kotlin/pers/acp/test/TestBean.kt
@@ -0,0 +1,22 @@
+package pers.acp.test
+
+import com.thoughtworks.xstream.annotations.XStreamAlias
+
+/**
+ * 请求系统头
+ */
+@XStreamAlias("root")
+data class TestBean(
+        @XStreamAlias("fileFlag")
+        var fileFlag: String? = null,
+        @XStreamAlias("BODY")
+        var body: Body = Body()
+) {
+    data class Body(
+            /**
+             * 手机号码
+             */
+            @XStreamAlias("mobile")
+            var mobile: String? = null
+    )
+}
