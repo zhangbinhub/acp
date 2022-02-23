a1f23d5b9c5c3e08af08278688fe3be3a141525c
zhangbin1010@qq.com <zb123456>
2022-02-23 11:05:06 +0800

    [Upgrade] v2021.0.0-M8

--- boot/acp-spring-boot-starter/src/test/kotlin/io/github/zhangbinhub/acp/boot/test/Test.kt
+++ boot/acp-spring-boot-starter/src/test/kotlin/io/github/zhangbinhub/acp/boot/test/Test.kt
@@ -1,6 +1,9 @@
 package io.github.zhangbinhub.acp.boot.test
 
 import io.github.zhangbinhub.acp.boot.tools.IpTools
+import org.springframework.http.server.PathContainer
+import org.springframework.util.AntPathMatcher
+import org.springframework.web.util.pattern.PathPatternParser
 
 /**
  * Create by zhangbin on 2017-12-19 11:28
@@ -8,4 +11,17 @@ import io.github.zhangbinhub.acp.boot.tools.IpTools
 fun main() {
     println(IpTools.getMACAddressFromIp("fe80::3113:a293:fccc:b73"))
     println(IpTools.getMACAddressFromIp("172.26.64.1"))
+
+    val antPathMatcher = AntPathMatcher()
+    val expression = "/url/{id:a|b|c}"
+    val urls = listOf("/url/a", "/url/b", "/url/c", "/url/d")
+    println("模式表达式：$expression")
+    urls.forEach {
+        println("[AntPathMatcher] 路径：$it 匹配结果：${antPathMatcher.match(expression, it)}")
+    }
+
+    val pattern = PathPatternParser().parse(expression)
+    urls.forEach {
+        println("[PathPatternParser] 路径：$it 匹配结果：${pattern.matches(PathContainer.parsePath(it))}")
+    }
 }
